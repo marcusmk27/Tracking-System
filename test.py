eb83e6ac79840bbf4ef585fa5b16a019ee7c1e57
marcusmk27 <129942447+marcusmk27@users.noreply.github.com>
2024-12-05 09:01:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -171,25 +171,10 @@ def main():
         try:
 
     # Handle pages
-    if page == 'View Current Issues':
-        try:
-            data, columns = view_all_issues()
-            if not data or not columns:
-                st.error("No data or columns found!")
-            else:
-                df = pd.DataFrame(data, columns=columns)
-                dffiltered = st.text_input("Enter issue code to filter:")
-                if 'issue_code' in df.columns:
-                    src_btn = st.button("Search")
-                    if src_btn:
-                        df_fil = df[df['issue_code'] == dffiltered]
-                        st.table(df_fil)
-                    else:
-                        st.table(df.head(5))
-                else:
-                    st.error("The required column 'issue_code' does not exist in the dataset!")
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+    if page == "View Current Issues":
+        st.header("Current Issues")
+        issues_df = view_all_issues()
+        st.write(issues_df.head(15))
 
     if page == "Login":
         st.header("Login")
